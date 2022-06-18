641c9967a93852ac3d5a533585ca46c0a1803530
Raul Sauco <sauco.raul@gmail.com>
2022-06-18 21:17:23 +0200

    Improve two sum solution

--- leetcode/two_sum.py
+++ leetcode/two_sum.py
@@ -1,13 +1,29 @@
 from typing import List
 
+# Solution using a hashmap has a O(n) for time and space.
+# Runtime: 104 ms, faster than 52.66% of Python3 online submissions for Two Sum.
+# Memory Usage: 15.1 MB, less than 49.66 % of Python3 online submissions for Two Sum.
+
+# Solution iterating through each item of the array has a complexity O(n^2) because it runs
+# through the entire array through each item on it.
+
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for index in range(len(nums)):
-            for index2 in range(index + 1, len(nums)):
-                if nums[index] + nums[index2] == target:
-                    return [index, index2]
+        s = {}
+        for i in range(len(nums)):
+            c = target - nums[i]
+            if c in s:
+                return [s[c], i]
+            s[nums[i]] = i
 
-        return 0
+        # O(n^2) because it runs through the entire array through each item on it
+        # for index in range(len(nums)):
+        #     for index2 in range(index + 1, len(nums)):
+        #         if nums[index] + nums[index2] == target:
+        #             return [index, index2]
+
+        # return 0
 
 def test():
     sol = Solution()
