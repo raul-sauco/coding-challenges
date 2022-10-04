925abc6bdb50504ce10800766270122f12c3bbd9
Raul Sauco <sauco.raul@gmail.com>
2022-10-04 14:39:23 +0200

    LeetCode 268. Missing Number add naive solution

--- leetcode/missing-number.py
+++ leetcode/missing-number.py
@@ -9,6 +9,28 @@
 import timeit
 from typing import List
 
+# 10e5 calls
+# » Set                 0.01628   seconds
+# » O1WithIndexes       0.01233   seconds
+
+
+# Create a set that contains all the values that should be in the range,
+# then iterate over the input removing values we see from the set, the
+# only remaining value will be the missing value. This is the easy
+# naive solution that I would suggest first in an interview.
+#
+# Time complexity: O(n) - To iterate over the elements.
+# Space complexity: O(n) - For the set.
+#
+# Runtime: 376 ms, faster than 19.67%
+# Memory Usage: 15.9 MB, less than 6.62%
+class Set:
+    def missingNumber(self, nums: List[int]) -> int:
+        s = {i for i in range(len(nums) + 1)}
+        for num in nums:
+            s.remove(num)
+        return s.pop()
+
 
 # Iterate over the input adding index values and subtracting input
 # values to and from a result. Once we add/subtract all values, the
@@ -19,7 +41,7 @@ from typing import List
 #
 # Runtime: 139 ms, faster than 94.32%
 # Memory Usage: 15.1 MB, less than 98.59%
-class Solution:
+class O1WithIndexes:
     def missingNumber(self, nums: List[int]) -> int:
         # There is one less element than value, the missing element.
         res = len(nums)
@@ -34,8 +56,12 @@ class Solution:
 
 
 def test():
-    executors = [Solution]
+    executors = [
+        Set,
+        O1WithIndexes,
+    ]
     tests = [
+        [[1], 0],
         [[3, 0, 1], 2],
         [[0, 1], 2],
         [[9, 6, 4, 2, 3, 5, 7, 0, 1], 8],
