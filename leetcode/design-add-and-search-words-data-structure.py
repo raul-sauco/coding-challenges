68243233f6733871359d720e52004184113860b2
Raul Sauco <sauco.raul@gmail.com>
2022-11-05 10:47:36 +0100

    LeetCode 211. Design Add and Search Words Data Structure reformat

--- leetcode/design-add-and-search-words-data-structure.py
+++ leetcode/design-add-and-search-words-data-structure.py
@@ -1,39 +1,112 @@
+# 211. Design Add and Search Words Data Structure
+# 🟠 Medium
+#
 # https://leetcode.com/problems/design-add-and-search-words-data-structure/
+#
+# Tags: String - Depth-First Search - Design - Trie
 
-from data import TrieNode
+
+import timeit
+from collections import defaultdict
 
 
-# Easier to read version of the word dictionary that uses TriedNode as a node
-# Runtime: 16930 ms, faster than 27.61% of Python3 online submissions for Design Add and Search Words Data Structure.
-# Memory Usage: 80 MB, less than 12.93 % of Python3 online submissions for Design Add and Search Words Data Structure.
-class WordDictionaryEasyRead:
+# Implement the word dictionary using plain hash maps to store the
+# children of each node. Inserting is exactly the same as with the
+# regular trie class and can be done in O(n), searching will need to
+# branch out to all the nodes' children when the current character is a
+# '.' wildcard and it could potentially take O(m*n) which is visiting
+# each node in the trie, even though usually would be faster.
+#
+# Time complexity: O(m*n) - For searching strings with wildcards, it
+# could potentially search the entire trie. O(n) for inserts, it will
+# perform one O(1) operation per character.
+# Space complexity: O(m*n) - Potentially, each character of each node
+# inserted could make its own node, in average it will be less than that
+# because words with common roots will share nodes.
+#
+# Runtime: 16919 ms, faster than 27.72%
+# Memory Usage: 55.9 MB, less than 87.04%
+class WordDictionary:
+    def __init__(self):
+        self.root = {}
 
+    def addWord(self, word: str) -> None:
+        current = self.root
+        for w in word:
+            if w not in current:
+                current[w] = {}
+            current = current[w]
+        current["?"] = True
+
+    def search(self, word: str) -> bool:
+        def searchFromNode(index, node) -> bool:
+            current = node
+            for i in range(index, len(word)):
+                w = word[i]
+                # Handle wildcards
+                if w == ".":
+                    # Recursive call to all the children with the sliced word
+                    for key, child in current.items():
+                        if key != "?" and searchFromNode(i + 1, child):
+                            return True
+                    return False
+                else:
+                    if w not in current:
+                        return False
+                    current = current[w]
+            return current.get("?", False)
+
+        return searchFromNode(0, self.root)
+
+
+# Definition for a trie node
+class TrieNode:
+    def __init__(self):
+        self.children = defaultdict(TrieNode)
+        self.isWord = False
+
+
+# Easier to read version of the word dictionary that uses TriedNode as a
+# node instead of a plain dictionary.
+#
+# Time complexity: O(m*n) - For searching strings with wildcards, it
+# could potentially search the entire trie. O(n) for inserts, it will
+# perform one O(1) operation per character.
+# Space complexity: O(m*n) - Potentially, each character of each node
+# inserted could make its own node, in average it will be less than that
+# because words with common roots will share nodes.
+#
+# Runtime: 16930 ms, faster than 27.61%
+# Memory Usage: 80 MB, less than 12.93%
+class WDEasyRead:
     def __init__(self):
         self.root = TrieNode()
 
     def addWord(self, word: str) -> None:
         current = self.root
-        # Recursively add the nodes
+        # Recursively add the nodes.
         for w in word:
             current = current.children[w]
-        # Mark the last character of the word as the end of a word
+        # Mark the last character of the word as the end of a word.
         current.isWord = True
 
     def search(self, word: str) -> bool:
         def dfs(node, word) -> bool:
-            # If our query string is longer than this branch of the exploration, return False
+            # If our query string is longer than this branch of the
+            # exploration, return False.
             if node:
-                # If we have exhausted the characters in the search word, exit
-                # Return True if we are at a word, False otherwise
-                # False returns will be ignored by the caller
+                # If we have exhausted the characters in the search
+                # word, exit.
+                # Return True if we are at a word, False otherwise.
+                # False returns will be ignored by the caller.
                 if len(word) == 0:
                     if node.isWord:
                         return True
 
-                # If we have not exhausted all the characters
+                # If we have not exhausted all the characters.
                 else:
-                    # We still have characters to explore in word
-                    if word[0] == '.':
+                    # We still have characters to explore in word.
+                    if word[0] == ".":
                         for n in node.children.values():
                             if dfs(n, word[1:]):
                                 return True
@@ -46,81 +119,56 @@ class WordDictionaryEasyRead:
         return res if res else False
 
 
-def testEasyRead():
-    wd = WordDictionaryEasyRead()
-    wd.addWord("bad")
-    wd.addWord("dad")
-    wd.addWord("mad")
-    assert wd.search("pad") == False
-    assert wd.search("bad") == True
-    assert wd.search(".ad") == True
-    assert wd.search("b..") == True
-    assert wd.search("..d") == True
-    assert wd.search("..e") == False
-    assert wd.search("...") == True
-    assert wd.search("bade") == False
-    assert wd.search("....") == False
-    assert wd.search(".") == False
-
-
-testEasyRead()
-
-
-# Runtime: 16919 ms, faster than 27.72% of Python3 online submissions for Design Add and Search Words Data Structure.
-# Memory Usage: 55.9 MB, less than 87.04 % of Python3 online submissions for Design Add and Search Words Data Structure.
-class WordDictionary:
-
-    def __init__(self):
-        self.root = {}
-
-    def addWord(self, word: str) -> None:
-        current = self.root
-        for w in word:
-            if w not in current:
-                current[w] = {}
-            current = current[w]
-        current['?'] = True
-
-    def search(self, word: str) -> bool:
-        def searchFromNode(index, node) -> bool:
-            current = node
-            for i in range(index, len(word)):
-                w = word[i]
-                # Handle wildcards
-                if w == '.':
-                    # Recursive call to all the children with the sliced word
-                    for key, child in current.items():
-                        if key != "?" and searchFromNode(i+1, child):
-                            return True
-                    return False
-                else:
-                    if w not in current:
-                        return False
-                    current = current[w]
-            return current.get('?', False)
-
-        return searchFromNode(0, self.root)
-
-        # Your WordDictionary object will be instantiated and called as such:
-        # obj = WordDictionary()
-        # obj.addWord(word)
-        # param_2 = obj.search(word)
-
-
 def test():
-    wd = WordDictionary()
-    wd.addWord("bad")
-    wd.addWord("dad")
-    wd.addWord("mad")
-    assert wd.search("pad") == False
-    assert wd.search("bad") == True
-    assert wd.search(".ad") == True
-    assert wd.search("b..") == True
-    assert wd.search("..d") == True
-    assert wd.search("..e") == False
-    assert wd.search("...") == True
-    assert wd.search("bade") == False
-    assert wd.search("....") == False
+    executors = [
+        WordDictionary,
+        WDEasyRead,
+    ]
+    tests = [
+        [
+            [
+                "WordDictionary",
+                "addWord",
+                "addWord",
+                "addWord",
+                "search",
+                "search",
+                "search",
+                "search",
+            ],
+            [
+                [],
+                ["bad"],
+                ["dad"],
+                ["mad"],
+                ["pad"],
+                ["bad"],
+                [".ad"],
+                ["b.."],
+            ],
+            [None, None, None, None, False, True, True, True],
+        ],
+    ]
+    for executor in executors:
+        start = timeit.default_timer()
+        for _ in range(1):
+            for col, t in enumerate(tests):
+                # The constructor does not take any parameters.
+                sol = executor()
+                for i in range(1, len(t[0])):
+                    call = t[0][i]
+                    # All the methods take exactly one parameter.
+                    result = getattr(sol, call)(t[1][i][0])
+                    exp = t[2][i]
+                    assert result == exp, (
+                        f"\033[93m» {result} <> {exp}\033[91m for"
+                        + f" test {col} using \033[1m{executor.__name__}"
+                    )
+        stop = timeit.default_timer()
+        used = str(round(stop - start, 5))
+        cols = "{0:20}{1:10}{2:10}"
+        res = cols.format(executor.__name__, used, "seconds")
+        print(f"\033[92m» {res}\033[0m")
 
 
 test()
