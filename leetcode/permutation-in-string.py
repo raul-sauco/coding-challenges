f60fa50b78da942771c14badf0278a6576c1c1af
Raul Sauco <sauco.raul@gmail.com>
2023-02-04 09:55:11 +0100

    LeetCode 567. Permutation in String reformat

--- leetcode/permutation-in-string.py
+++ leetcode/permutation-in-string.py
@@ -8,40 +8,48 @@
 import timeit
 from collections import defaultdict
 
-# A very similar problem to LeetCode 438. Find All Anagrams in a String. We can use the same technique:
-# Use a hashmap to keep track of characters that we need vs characters we have seen.
-# Negative values denote characters in s1 that we haven't found in s2 yet, positive values denote characters that we
-# have seen in s2 but aren't, or not in the same frequency, in s1. When we have the same frequency of the same
-# character, we remove the entry from the dictionary.
-# We use a sliding window of size s1 to iterate over s2 adding the newly seen right character and removing the left.
-# If at some point the frequency dictionary is empty, we have a full match and can return True, if we get to the
-# end of s2 without a match, we can return False.
+
+# A very similar problem to LeetCode 438. Find All Anagrams in a String.
+# We can use the same technique: use a hashmap to keep track of
+# characters that we need vs characters we have seen, negative values
+# denote characters in s1 that we haven't found in s2 yet, positive
+# values denote characters that we have seen in s2 but aren't, or not in
+# the same frequency, in s1. When we have the same frequency of the same
+# character, we remove the entry from the dictionary. We use a sliding
+# window of size s1 to iterate over s2 adding the newly seen right
+# character and removing the left. If at some point the frequency
+# dictionary is empty, we have a full match and can return True, if we
+# get to the end of s2 without a match, we can return False.
 #
-# Time complexity: O(n) - with n being the len(s2)
-# Space complexity: O(n) - with n being the len(s1)
+# Time complexity: O(n) - Where n is the number of characters in the s2
+# we iterate over all characters in s2 and do O(1) work for each.
+# Space complexity: O(m) - Where m is the number of characters in s1,
+# the frequency dictionary would have m entries if all the characters in
+# s1 had frequency 1.
 #
-# Runtime: 61 ms, faster than 98.26% of Python3 online submissions for Permutation in String.
-# Memory Usage: 13.9 MB, less than 68.06% of Python3 online submissions for Permutation in String.
-class Solution:
+# Runtime: 61 ms, faster than 98.26%
+# Memory Usage: 13.8 MB, less than 94.40%
+class SlidingWindow:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         if len(s1) > len(s2):
             return False
+        # For some reason the defaultdict is faster than Counter.
+        # freq = Counter()
         freq = defaultdict(int)
-        # Mark each character in s1 as "need to match"
+        # Mark each character in s1 as "need to match".
         for c in s1:
             freq[c] -= 1
-
-        # Mark the characters in the first len(s1) characters on s2 as "seen"
+        # Mark the characters in the first len(s1) characters on s2 as
+        # "seen".
         for c in s2[: len(s1)]:
             freq[c] += 1
             if freq[c] == 0:
                 del freq[c]
-
         # Check if we have a match
         if not freq:
             return True
-
-        # Iterate over the rest of the string s2 "unseeing" the left character and "seeing" the right one.
+        # Iterate over the rest of the string s2 "unseeing" the left
+        # character and "seeing" the right one.
         left, right = 0, len(s1) - 1
         while right < len(s2) - 1:
             freq[s2[left]] -= 1
@@ -55,19 +63,21 @@ class Solution:
             # If at any point we match s1, return True
             if not freq:
                 return True
-
         # If none of the substrings matched, return False
         return False
 
 
 def test():
-    executors = [Solution]
+    executors = [
+        SlidingWindow,
+    ]
     tests = [
+        ["aba", "ab", False],
+        ["adc", "dcda", True],
         ["ab", "eidbaooo", True],
         ["ab", "eidboaoo", False],
-        ["ababaa", "ab", False],  # longer s1
-        ["aabbcc", "cacbba", True],  # Full match
-        ["aabcc", "cacbba", False],  # Order mismatch
+        ["aabbcc", "cacbba", True],
+        ["aabcc", "cacbba", False],
     ]
     for executor in executors:
         start = timeit.default_timer()
@@ -76,12 +86,14 @@ def test():
                 sol = executor()
                 result = sol.checkInclusion(t[0], t[1])
                 exp = t[2]
-                assert (
-                    result == exp
-                ), f"\033[93m» {result} <> {exp}\033[91m for test {col} using \033[1m{executor.__name__}"
+                assert result == exp, (
+                    f"\033[93m» {result} <> {exp}\033[91m for"
+                    + f" test {col} using \033[1m{executor.__name__}"
+                )
         stop = timeit.default_timer()
         used = str(round(stop - start, 5))
-        res = "{0:20}{1:10}{2:10}".format(executor.__name__, used, "seconds")
+        cols = "{0:20}{1:10}{2:10}"
+        res = cols.format(executor.__name__, used, "seconds")
         print(f"\033[92m» {res}\033[0m")
 
 
