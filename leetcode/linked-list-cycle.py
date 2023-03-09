1926959f64f5f1bedb69fabc5620ea2d3e823097
Raul Sauco <sauco.raul@gmail.com>
2023-03-09 11:15:31 +0100

    LeetCode 141. Linked List Cycle reformat

--- leetcode/linked-list-cycle.py
+++ leetcode/linked-list-cycle.py
@@ -1,38 +1,54 @@
+# 141. Linked List Cycle
+# 🟢 Easy
+#
 # https://leetcode.com/problems/linked-list-cycle/
+#
+# Tags: Hash Table - Linked List - Two Pointers
 
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
 
 from typing import Optional
-from data import ListNode
+
+from utils.linked_list import ListNode
 
 
-# Runtime: 95 ms, faster than 33.27% of Python3 online submissions for Linked List Cycle.
-# Memory Usage: 17.5 MB, less than 66.33 % of Python3 online submissions for Linked List Cycle.
+# Use the fast and slow pointer cycle detection algorithm.
+# https://en.wikipedia.org/wiki/Cycle_detection
+#
+# Time complexity: O(n) - The complexity is linear over the number of
+# nodes in the list.
+# Space complexity: O(1) - We use constant extra space.
+#
+# Runtime 48 ms Beats 95.39%
+# Memory 17.5 MB Beats 93.78%
 class Floyd:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        slow = fast = head
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
-            if slow == fast:
-                return True
-        return False
+        if not head or not head.next:
+            return False
+        slow, fast = head.next, head.next.next
+        while fast and fast.next and slow != fast:
+            slow, fast = slow.next, fast.next.next
+        return slow == fast
 
 
-# Runtime: 103 ms, faster than 22.79% of Python3 online submissions for Linked List Cycle.
-# Memory Usage: 17.9 MB, less than 10.43 % of Python3 online submissions for Linked List Cycle.
+# If the list nodes are hashable, or if they have unique values that we
+# can hash, we can use a set, or a hashmap of value: node, to add
+# nodes to, if we see a node that is already in the hash set, we can
+# return true because that is the start of the cycle.
+#
+# Time complexity: O(n) - Linear time if we consider hashing O(1).
+# Space complexity: O(n) - The set can grow to size n.
+#
+# Runtime 45 ms Beats 98.30%
+# Memory 17.9 MB Beats 8.15%
 class Set:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         if not head:
             return False
-        seen = {head}
-        while head.next:
-            head = head.next
-            if head in seen:
-                return True
-            seen.add(head)
-        return False
+        node, seen = head, set()
+        while node.next and node not in seen:
+            seen.add(node)
+            node = node.next
+        return node in seen
+
+
+print(f"\033[93m» This file does not have any tests\033[0m")
