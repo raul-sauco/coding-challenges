40eb86e417a3cb455b76e454b8ae108455085709
Raul Sauco <sauco.raul@gmail.com>
2023-03-09 09:35:46 +0100

    LeetCode 142. Linked List Cycle II reformat

--- leetcode/linked-list-cycle-ii.py
+++ leetcode/linked-list-cycle-ii.py
@@ -1,19 +1,52 @@
+# 142. Linked List Cycle II
+# 🟠 Medium
+#
 # https://leetcode.com/problems/linked-list-cycle-ii/
+#
+# Tags: Hash Table - Linked List - Two Pointers
 
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
 
 from typing import Optional
 
-from data import ListNode
-
-# Runtime: 61 ms, faster than 77.22% of Python3 online submissions for Linked List Cycle II.
-# Memory Usage: 18 MB, less than 9.90 % of Python3 online submissions for Linked List Cycle II.
+from utils.linked_list import ListNode
 
 
+# Use the fast and slow pointer cycle detection algorithm.
+# https://en.wikipedia.org/wiki/Cycle_detection
+#
+# Time complexity: O(n) - The complexity is linear over the number of
+# nodes in the list.
+# Space complexity: O(1) - We use constant extra space.
+#
+# Runtime 50 ms Beats 72.17%
+# Memory 17.2 MB Beats 90.55%
+class Floyd:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = fast = head
+        while fast and fast.next:
+            slow, fast = slow.next, fast.next.next
+            if slow == fast:
+                break
+        # If the loop exits and the pointers do not point to the same
+        # node, there is no cycle.
+        else:
+            return None
+        fast = head
+        while fast != slow:
+            slow, fast = slow.next, fast.next
+        return fast
+
+
+# If the list nodes are hashable, or if they have unique values that we
+# can hash, we can use a set, or a hashmap of value: node, to add
+# nodes to, if we see a node that is already in the hash set, we can
+# return it because that is the start of the cycle.
+#
+# Time complexity: O(n) - Linear time if we consider hashing O(1).
+# Space complexity: O(n) - The set can grow to size n.
+#
+# Runtime 55 ms Beats 52.57%
+# Memory 17.9 MB Beats 21.35%
 class Set:
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head:
@@ -28,19 +61,4 @@ class Set:
         return None
 
 
-# Runtime: 95 ms, faster than 24.21% of Python3 online submissions for Linked List Cycle II.
-# Memory Usage: 17.3 MB, less than 57.81 % of Python3 online submissions for Linked List Cycle II.
-class Floyd:
-    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        slow = fast = head
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
-            if slow == fast:
-                break
-        else:
-            return None
-        while head != slow:
-            head = head.next
-            slow = slow.next
-        return head
+print(f"\033[93m» This file does not have any tests\033[0m")
