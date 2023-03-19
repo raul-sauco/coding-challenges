16e75a0e6c15d1584f5bbbf1fafe67c2b81ff9a4
Raul Sauco <sauco.raul@gmail.com>
2023-03-19 12:43:55 +0100

    LeetCode 4. Median of Two Sorted Arrays Rust (D&C)

--- leetcode/median-of-two-sorted-arrays.rs
+++ leetcode/median-of-two-sorted-arrays.rs
@@ -8,6 +8,90 @@
 struct Solution;
 impl Solution {
     /**
+     * Use a divide-and-conquer algorithm, compute the positions on the merged
+     * array that we are looking for and have an auxiliary function return the
+     * value at that position, or the average between the surrounding positions.
+     * Each iteration of the function discards half of one of the input arrays
+     * until there is only one element being considered between both of them.
+     *
+     * Time complexity: O(log(m+n)) - At each iteration the algorithm discards
+     * half of the search space, the initial search space is both arrays.
+     * Space complexity: O(log(m+n)) - The call stack will hold one call for
+     * each time we discard half of the search space.
+     *
+     * Runtime 0 ms Beats 100%
+     * Memory 2.2 MB Beats 56.11%
+     */
+    pub fn find_median_sorted_arrays(nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
+        // If one of the arrays is empty, return the median from the non-empty one.
+        if nums1.is_empty() || nums2.is_empty() {
+            return Self::median_of_sorted_array(if nums1.is_empty() { &nums2 } else { &nums1 })
+                as f64;
+        }
+        let (a, b) = (&nums1, &nums2);
+        let (m, n, l) = (a.len(), b.len(), a.len() + b.len());
+        let (la, lb, ra, rb) = (0, 0, m - 1, n - 1);
+        if l % 2 == 0 {
+            (Self::kth_smallest(a, b, la, ra, lb, rb, l / 2)
+                + Self::kth_smallest(a, b, la, ra, lb, rb, l / 2 - 1))
+                / 2.0
+        } else {
+            Self::kth_smallest(a, b, la, ra, lb, rb, l / 2)
+        }
+    }
+
+    /**
+     * A function that returns the k-th smallest element given two sorted arrays.
+     */
+    pub fn kth_smallest(
+        a: &Vec<i32>,
+        b: &Vec<i32>,
+        la: usize,
+        ra: usize,
+        lb: usize,
+        rb: usize,
+        k: usize,
+    ) -> f64 {
+        if la > ra {
+            return b[k - la] as f64;
+        }
+        if lb > rb {
+            return a[k - lb] as f64;
+        }
+        let (ma, mb) = ((la + ra) / 2, (lb + rb) / 2);
+        if ma + mb < k {
+            if a[ma] > b[mb] {
+                return Self::kth_smallest(a, b, la, ra, mb + 1, rb, k);
+            }
+            return Self::kth_smallest(a, b, ma + 1, ra, lb, rb, k);
+        }
+        if a[ma] > b[mb] {
+            // Prevent subtracting below 0.
+            if ma == 0 {
+                return b[k - la] as f64;
+            }
+            return Self::kth_smallest(a, b, la, ma - 1, lb, rb, k);
+        }
+        // Prevent subtracting below 0
+        if mb == 0 {
+            return a[k - lb] as f64;
+        }
+        return Self::kth_smallest(a, b, la, ra, lb, mb - 1, k);
+    }
+
+    /**
+     * A function that returns the median of one sorted array.
+     */
+    pub fn median_of_sorted_array(a: &Vec<i32>) -> f64 {
+        let mid = a.len() / 2;
+        if a.len() % 2 == 0 {
+            (a[mid] + a[mid - 1]) as f64 / 2.0
+        } else {
+            a[mid] as f64
+        }
+    }
+
+    /**
      * Use binary search, we choose the smaller input array, or either if the
      * same size, and do binary search for the edge of the partition, we try
      * positions and adjust the boundary left or right based on whether we
@@ -89,6 +173,10 @@ fn main() {
         ),
     ];
     for test in tests {
+        assert_eq!(
+            Solution::find_median_sorted_arrays(test.0.clone(), test.1.clone()),
+            test.2
+        );
         assert_eq!(
             Solution::find_median_sorted_arrays_bs(test.0, test.1),
             test.2
