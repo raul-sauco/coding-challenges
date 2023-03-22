29fd1141e84ac9b261c9421e0a007b9856d8e44a
Raul Sauco <sauco.raul@gmail.com>
2023-03-22 23:28:02 +0100

    Add HackerRank Java template Solution.java

---
+++ hackerrank/Solution.java
@@ -0,0 +1,13 @@
+// Problem Name
+// 🟠 Medium
+//
+// https://www.hackerrank.com/challenges/problem-name
+//
+// Tags:
+
+public class Solution {
+
+  public static void main(String[] args) {
+    // todo fill in
+  }
+}
