7802ca4be20b596baf402470a20e1c2bb46933eb
Raul Sauco <sauco.raul@gmail.com>
2023-03-29 22:07:04 +0200

    LC 347. Top K Frequent Elements (Bucket Sort)

--- leetcode/top-k-frequent-elements.py
+++ leetcode/top-k-frequent-elements.py
@@ -7,7 +7,7 @@
 #  - Heap (Priority Queue) - Bucket Sort - Counting - Quickselect
 
 import timeit
-from collections import Counter
+from collections import Counter, defaultdict
 from typing import List
 
 
@@ -19,13 +19,54 @@ from typing import List
 #
 # Runtime 95 ms Beats 95.19%
 # Memory Usage 18.4 Beats 99.66%
-class Solution:
+class UseCounter:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         return [x[0] for x in Counter(nums).most_common(k)]
 
 
+# Use bucket sorting, first get the frequency of items in the input,
+# then create an array of buckets and place each unique value in the
+# array indexed by its frequency. To create the result, iterate in
+# reverse over the array indexes and pop elements until the result has
+# grown to have k elements.
+#
+# Time complexity: O(n) - Sorting the dictionary by value.
+# Space complexity: O(n) - The size of the dictionary.
+#
+# Runtime 104 ms Beats 71.15%
+# Memory Usage 18.7 Beats 42.44%
+class BucketSort:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # Count the frequency of each element, same as Counter(nums).
+        freq = defaultdict(int)
+        for num in nums:
+            freq[num] += 1
+        # A bucket that holds elements indexed by their frequency.
+        bucket = []
+        for num, count in freq.items():
+            # Expand bucket if needed.
+            while count + 1 > len(bucket):
+                bucket.append(None)
+            # If empty add a list at the index.
+            if not bucket[count]:
+                bucket[count] = []
+            # Append the value indexed by its frequency.
+            bucket[count].append(num)
+        # Create a result of size k.
+        res, idx = [], len(bucket) - 1
+        while len(res) < k:
+            if bucket[idx]:
+                res.append(bucket[idx].pop())
+            else:
+                idx -= 1
+        return res
+
+
 def test():
-    executors = [Solution]
+    executors = [
+        UseCounter,
+        BucketSort,
+    ]
     tests = [
         [[1], 1, [1]],
         [[3, 0, 1, 0], 1, [0]],
