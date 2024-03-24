6156fbed7d0a7daa8632a653fa591d2529b5032a
Raul Sauco <sauco.raul@gmail.com>
2024-03-24 07:44:57 +0100

    LC 287. Find the Duplicate Number (Rust) Revamp

--- leetcode/find-the-duplicate-number.rs
+++ leetcode/find-the-duplicate-number.rs
@@ -16,9 +16,10 @@ impl Solution {
     /// Time complexity: O(n) - Linear time complexity.
     /// Space complexity: O(1) - We use constant extra space.
     ///
-    /// Runtime 16 ms Beats 21.90%
-    /// Memory 4 MB Beats 20.95%
-    pub fn find_duplicate(nums: Vec<i32>) -> i32 {
+    /// Runtime 11 ms Beats 59%
+    /// Memory 4 MB Beats 24%
+    #[allow(dead_code)]
+    pub fn find_duplicate_cast(nums: Vec<i32>) -> i32 {
         let nums = nums.iter().map(|x| *x as usize).collect::<Vec<usize>>();
         let (mut slow, mut fast) = (nums[0], nums[nums[0]]);
         while slow != fast {
@@ -33,13 +34,60 @@ impl Solution {
         }
         slow as i32
     }
+
+    /// Same logic but do not modify the input vector to cast to usize.
+    ///
+    /// Time complexity: O(n) - Linear time complexity.
+    /// Space complexity: O(1) - We use constant extra space.
+    ///
+    /// Runtime 12 ms Beats 57%
+    /// Memory 3.10 MB Beats 83%
+    pub fn find_duplicate(nums: Vec<i32>) -> i32 {
+        let (mut slow, mut fast) = (nums[0], nums[nums[0] as usize]);
+        while slow != fast {
+            slow = nums[slow as usize];
+            fast = nums[nums[fast as usize] as usize];
+        }
+        // Reuse one of the pointers, it can be either.
+        fast = 0;
+        while slow != fast {
+            slow = nums[slow as usize];
+            fast = nums[fast as usize];
+        }
+        fast as i32
+    }
 }
 
 // Tests.
 fn main() {
-    let tests = [(vec![1, 3, 4, 2, 2], 2), (vec![3, 1, 3, 4, 2], 3)];
-    for t in tests {
-        assert_eq!(Solution::find_duplicate(t.0), t.1);
+    let tests = [
+        (vec![1, 3, 4, 2, 2], 2),
+        (vec![3, 1, 3, 4, 2], 3),
+        (vec![3, 3, 3, 3, 3], 3),
+    ];
+    println!("\n\x1b[92m» Running {} tests...\x1b[0m", tests.len());
+    let mut success = 0;
+    for (i, t) in tests.iter().enumerate() {
+        let res = Solution::find_duplicate(t.0.clone());
+        if res == t.1 {
+            success += 1;
+            println!("\x1b[92m✔\x1b[95m Test {} passed!\x1b[0m", i);
+        } else {
+            println!(
+                "\x1b[31mx\x1b[95m Test {} failed expected: {:?} but got {}!!\x1b[0m",
+                i, t.1, res
+            );
+        }
     }
-    println!("\x1b[92m» All tests passed!\x1b[0m")
+    println!();
+    if success == tests.len() {
+        println!("\x1b[30;42m✔ All tests passed!\x1b[0m")
+    } else if success == 0 {
+        println!("\x1b[31mx \x1b[41;37mAll tests failed!\x1b[0m")
+    } else {
+        println!(
+            "\x1b[31mx\x1b[95m {} tests failed!\x1b[0m",
+            tests.len() - success
+        )
+    }
 }
